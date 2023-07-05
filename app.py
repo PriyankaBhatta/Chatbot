8d55b8812d7a92f29608cdff9a496732e3c08232
Priyanka Bhatta <priyankabhatta2323@gmail.com>
2023-07-05 11:40:04 +0545

    resolving bugs

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
+import requests
 
 
 app = Flask(__name__)
@@ -9,13 +10,28 @@ def index():
     source_currency = data['queryResult']['parameters']['unit-currency']['currency']
     amount = data['queryResult']['parameters']['unit-currency']['amount']
     target_currency = data['queryResult']['parameters']['currency-name']
-    
-    
-    print(source_currency)
-    print(amount)
-    print(target_currency)
-    return "Hello"
 
+
+    cf = fetch_conversion_factor(source_currency, target_currency)
+    final_amount = amount * cf
+    final_amount = round(final_amount, 2)
+
+    response = {
+        'fulfillmentText' : "{} {} is {} {}".format(amount, source_currency, final_amount, target_currency)
+    }
+    print(final_amount)
+    return jsonify(response)
+
+def fetch_conversion_factor(source, target):
+    url = "https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey=9634365d28ff36a45de9".format(source,target)
+    
+    response = requests.get(url)
+    response = response.json()
+
+
+    return response['{}_{}'.format(source, target)]
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
